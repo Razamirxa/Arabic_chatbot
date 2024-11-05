3a162df3fe0010a463d2d67cf7355bf3590130f9
Raza UL Mustafa <122047009+Razamirxa@users.noreply.github.com>
2024-11-05 11:09:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_chat import message
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_community.document_loaders import TextLoader
 from langchain.document_loaders import Docx2txtLoader
